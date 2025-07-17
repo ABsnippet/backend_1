5807725c8313e6aa49e45d9efe88c9319591ba2e
ABsnippet <bandeamit610@gmail.com>
2025-07-17 11:37:55 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,7 +15,13 @@ app.use(express.json({limit:"18kb"}))
 app.use(express.urlencoded())
 app.use(express.static("public"))
 
-app.use(cookieParser())
+app.use(cookieParser());
 
+
+//import routes
+import userRouter from './routes/user.router.js';
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
 
 export default app ;//export {app}
